5cc7d57565ee96d745d6106cead5d9a61967bd4c
KaveeN <vpdkkaveen@gmail.com>
2025-03-08 17:23:05 +0530

    added FuntionHoisting.js file

---
+++ js/FuntionHoisting.js
@@ -0,0 +1,23 @@
+console.log("Function Hoisting is Loaded...");
+
+// greet();
+
+// function greet() {
+//     console.log("Hello World!");
+// }
+
+// =====================================
+
+// greet();
+
+// var greet = function() {
+//     console.log("Hello World!");
+// }
+
+// =====================================
+
+greet();
+
+let greet = function() {
+    console.log("Hello World!");
+}
