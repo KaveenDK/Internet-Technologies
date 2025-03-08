bc23528cfddc9c0566f2d514299974284db5b222
KaveeN <vpdkkaveen@gmail.com>
2025-03-08 09:28:32 +0530

    added mouseEventListeners

---
+++ js/EventListeners.js
@@ -0,0 +1,38 @@
+console.log("EventListeners.js loaded...");
+
+const box = document.querySelector('.box');
+
+// ===== 1. Single click event =====
+// box.addEventListener('click', function () {
+//     console.log("Box clicked");
+// })
+
+// ===== 2. Double click events =====
+// box.addEventListener('dblclick', function() {
+//     console.log("Double clicked the box");
+// })
+
+// ===== 3. Mouse down event =====
+// box.addEventListener('mousedown', function () {
+//     console.log("Mouse Pressed");
+// })
+
+// ===== 4. Mouse up event =====
+// box.addEventListener('mouseup', function () {
+//     console.log("Mouse released");
+// })
+
+// ===== 5. Mouse move event =====
+// box.addEventListener('mousemove', function () {
+//     console.log("Mouse moved");
+// })
+
+// ===== 6. Mouse over event =====
+// box.addEventListener('mouseover', function () {
+//     console.log("Mouse over the box");
+// })
+
+// ===== 7. Mouse out event =====
+// box.addEventListener('mouseout', function () {
+//     console.log("Mouse out of the box");
+// })
